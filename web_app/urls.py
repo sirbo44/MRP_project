09035f9efd5a22f06623fc6c152487573b95752c
Syrmpopoulos Stavros <stsirbp@gmail.com>
2025-03-26 19:20:47 +0200

    Created home, estimation_period, track_order, archive, forecasting, report pages

--- web_app/urls.py
+++ web_app/urls.py
@@ -4,5 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.to_login, name="to_login"),
-    path("login", views.login, name="login")
+    path("login", views.login, name="login"),
+    path("home", views.home, name="home"),
+    path("estimation_period", views.estimation_period, name="estimation_period"),
+    path("track_order", views.track_order, name="track_order"),
+    path("archive", views.archive, name="archive"),
+    path("forecasting", views.forecasting, name="forecasting"),
+    path("report", views.report, name="report"),
+    # path("", views., name=""),
 ]
--- web_app/views.py
+++ web_app/views.py
@@ -6,6 +6,29 @@ def to_login(request):
     return redirect('login')
 
 def login(request):
-    template = loader.get_template("login.html")
     return render(request, "login.html")
+
+def home(request):
+    context = {"page" : "home"}
+    return render(request, "home.html", context)
+
+def estimation_period(request):
+    context = {"page" : "estimation period"}
+    return render(request, "estimation_period.html", context)
+
+def track_order(request):
+    context = {"page" : "track order"}
+    return render(request, "track_order.html", context)
+
+def archive(request):
+    context = {"page" : "archive"}
+    return render(request, "archive.html", context)
+
+def forecasting(request):
+    context = {"page" : "forecasting"}
+    return render(request, "forecasting.html", context)
+
+def report(request):
+    context = {"page" : "report"}
+    return render(request, "report.html", context)
 
